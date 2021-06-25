2a20d03fb4fbfbd345af6d154632884ec49b1334
Belit <belitberdelk@gmail.com>
2021-06-26 01:21:04 +0300

    added sql answer4

---
+++ SQL/answer4.sql
@@ -0,0 +1,10 @@
+/*film tablosunda bulunan replacement_cost sütununda bulunan birbirinden farklı değerleri sıralayınız.*/
+SELECT DISTINCT  replacement_cost FROM film ;
+/*film tablosunda bulunan replacement_cost sütununda birbirinden farklı kaç tane veri vardır?*/
+SELECT count( DISTINCT  replacement_cost ) FROM film ;
+/*film tablosunda bulunan film isimlerinde (title) kaç tanesini T karakteri ile başlar ve aynı zamanda rating 'G' ye eşittir?*/
+SELECT  * FROM film WHERE title LIKE 'T%' AND rating = 'G' ;
+/*country tablosunda bulunan ülke isimlerinden (country) kaç tanesi 5 karakterden oluşmaktadır?*/
+SELECT COUNT (country) FROM country WHERE country LIKE '_____';
+/*city tablosundaki şehir isimlerinin kaçtanesi 'R' veya r karakteri ile biter?*/
+SELECT COUNT (city) FROM city WHERE city LIKE '%R' OR city LIKE '%r';
